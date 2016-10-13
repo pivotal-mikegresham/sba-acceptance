f87beb803e73465f8713bd405f5284592c363483
Mike Gresham <mgresham@pivotal.io>
2016-10-13 16:07:01 -0700

    first commit

---
+++ src/main/java/com/appsman/AcceptanceApplication.java
@@ -0,0 +1,12 @@
+package com.appsman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcceptanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcceptanceApplication.class, args);
+	}
+}
